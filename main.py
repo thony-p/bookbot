f093f0305b0aba3ed46da6e2bc17489c2358df6d
Thony <51670641+thony-p@users.noreply.github.com>
2024-02-19 21:59:12 +0000

    added letter count

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def main():
     path = "books/frank.txt"
-    contents = get_file(path)
-    word_number = wordcount(contents)
+    content = get_file(path)
+    word_number = wordcount(content)
     print(word_number)
+    letters = count_letters(content)
+    print(letters)
 
 def wordcount(contents):
     return len(contents.split())
@@ -10,5 +12,17 @@ def wordcount(contents):
 def get_file(path):
     with open(path) as f:
         return f.read()
+
+def count_letters(content):
+    dict = {}
+    lowercase = content.lower()
+    for char in lowercase:
+        if char in dict:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+    return dict
+
+
 
 main()
